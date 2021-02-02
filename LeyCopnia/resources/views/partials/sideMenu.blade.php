974c217be729f61307cd3b346d9c51b333d262c1
MiguelPasaje <cartel-dy@hotmail.com>
2021-02-02 12:18:08 -0500

    correciones diseño css nav y menuSide

--- LeyCopnia/resources/views/partials/sideMenu.blade.php
+++ LeyCopnia/resources/views/partials/sideMenu.blade.php
@@ -8,21 +8,21 @@
         <div class="menu">
            
             <a href="#" class=" iconSM d-block p-3 text-light mr-3 lead">
-                <i class="fas fa-dot-circle"></i>
+                <i class="lateral fas fa-dot-circle"></i>
                 &nbsp;&nbsp;&nbsp;Capitulos
             </a>
             <a href="#" class=" iconSM d-block p-3 text-light mr-2 lead">
-                <i class="fas fa-dot-circle"></i>
+                <i class="lateral fas fa-dot-circle"></i>
                 &nbsp;&nbsp;&nbsp;Articulos
             </a>
 
             <a href="#" class=" iconSM d-block p-3 text-light mr-3 lead">
-                <i class="fas fa-user"></i>
+                <i class="lateral fas fa-user"></i>
                 &nbsp;&nbsp;&nbsp; Perfil
             </a>
 
             <a href="#" class=" iconSM d-block p-3 text-light mr-3 lead">
-                <i class="fas fa-cog"></i>
+                <i class="lateral fas fa-cog"></i>
                 &nbsp;&nbsp;&nbsp; Configuración
             </a>
 
