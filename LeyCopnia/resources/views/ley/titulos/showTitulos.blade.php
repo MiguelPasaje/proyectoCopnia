3b2d9a9600a2db9c80a3ae61c2567096de4a21ab
MiguelPasaje <cartel-dy@hotmail.com>
2021-02-09 09:31:47 -0500

    cambios basicos en showTitulos

--- LeyCopnia/resources/views/ley/titulos/showTitulos.blade.php
+++ LeyCopnia/resources/views/ley/titulos/showTitulos.blade.php
@@ -2,12 +2,26 @@
 
 @section('title','TITULOS')
 
+<style>
+
+  .contenedor{
+    background-color:rgb(255, 255, 255);
+    margin-left:15%;
+    margin-right:15%;
+
+  }
+
+</style>
+
 
 
 @section('content')
 @include('partials.sideMenu')
-  <div class="container">
+
+  <div class="contenedor">
     Donde se muestra los titulos
+
+
   </div>
 
 @stop
