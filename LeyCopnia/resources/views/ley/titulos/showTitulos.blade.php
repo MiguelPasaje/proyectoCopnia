35042601b83ee3e577251587c550062235ae3574
Felipe Hidalgo <juanfehidalgo@gmail.com>
2021-02-12 16:33:14 -0500

    En showTitulos se añadio mejoras a la vista, y se modifico el tituloController.

--- LeyCopnia/resources/views/ley/titulos/showTitulos.blade.php
+++ LeyCopnia/resources/views/ley/titulos/showTitulos.blade.php
@@ -57,76 +57,104 @@
 @section('content')
 {{-- @include('partials.sideMenu') --}}
 
-  <div class="row">
-    
-    <div class="col-4">
-      {{-- row en la que se muestra el titulo de la ley --}}
-      <div class="row m-3">
-        <h1 class="shadow-lg p-3 mb-5 bg-body rounded ">
-          {{ $ley->ley }}
-        </h1> 
-        <hr><hr>
-      </div>
-      
-      {{-- row para mostrar los titulos y capitulos --}}
-      <div class="row m-3">
-        @foreach ($titulos as $titulo)    
-          {{-- titulos --}}
-          <p>
-            <a data-bs-toggle="collapse" href="{{url('#collapseCapitulos'. $titulo->idTitulo)}}" role="button" aria-expanded="false" aria-controls="collapseCapitulos">
-              <h3 class="ps-md-3 p  x-auto" >
-                {{ $titulo->titulo }}
-              </h3>
-            </a>
-            
-            <h6 class="ps-md-3">
-              {{ $titulo->titDes }}
-            </h6>
-          </p>
-            
-          {{-- capitulos --}}
-          <div class="collapse" id={{'collapseCapitulos'.$titulo->idTitulo}}>    
-            @foreach($consucap as  $con)
-              @if($con->idTitulo === $titulo->idTitulo)
-
-                <a data-bs-toggle="collapse" href="{{url('#collapseArticulos'.$con->idCapitulo)}}" role="button" aria-expanded="false" aria-controls="collapseArticulos">
-                  <h6 class="ps-md-4">{{ $con->capitulo}}</h6>
-                </a>
-                <h6 class="ps-md-4">{{ $con->capDes}}</h6>
-                <hr>
-              @endif
-            @endforeach
-          </div>        
-                    
-          {{--  --}}
-          <hr>
-        @endforeach
-
-      </div>
-  
-    </div>
-
-    {{-- articulos --}}
-    <div class="col-5">
-      <div class="row m-3">
+    <div class="row">
         
-        @foreach ($articulos as $articulo)
-        <div class="collapse" id={{'collapseArticulos'.$articulo->idCapitulo}}>
-          @foreach ($consuart as $con)
-            @if ($con->idArticulo === $articulo->idArticulo)
-              <p>{{$articulo->articulo}}</p>
-              <hr>
-            @endif
-          @endforeach    
+        <div class="col-4">
+            {{-- TITULO DE LA LEY --}}
+            <div class="row m-3">
+                <h1 class="shadow-lg p-3 mb-5 bg-body rounded ">
+                    {{ $ley->ley }}
+                </h1> 
+                <hr>
+            </div>
+        
+            {{-- Titulos & Capitulos --}}
+            <div class="row m-3">
+                @foreach ($titulos as $titulo)    
+                {{-- titulos --}}
+                        <a data-bs-toggle="collapse" href="{{url('#collapseCapitulos'. $titulo->idTitulo)}}" role="button" aria-expanded="false" aria-controls="collapseCapitulos">
+                            <h3 class="ps-md-3 p  x-auto" >
+                                {{ $titulo->titulo }}
+                            </h3>
+                        </a>
+                    
+                    <h6 class="ps-md-3">
+                        {{ $titulo->titDes }}
+                    </h6>
+                    
+                    {{-- capitulos --}}
+                    <div class="collapse" id={{'collapseCapitulos'.$titulo->idTitulo}}>
+                        <div class="list-group" id="list-tab" role="tablist">
+                            <a class="list-group-item  active link-danger" id="capitulo0" data-bs-toggle="list" href="#list-articulos" role="tab" aria-controls="articulos">Limpiar Seccion Articulos</a>
+                            @foreach($capitulos as  $cap)
+                                @if($cap->idTitulo === $titulo->idTitulo)
+                                    <a class="list-group-item " id={{"capitulos".$cap->idCapitulo}} href="{{url('#Articulos'.$cap->idCapitulo)}}" data-bs-toggle="list" role="tab" aria-controls="articulos">
+                                        <h6 class="ps-md-4">{{ $cap->capitulo}}</h6>
+                                        <h6 class="ps-md-4">{{ $cap->capDes}}</h6>
+                                    </a>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>                    
+                
+                    <hr>{{--  --}}
+                @endforeach
+            </div>
         </div>
-      @endforeach
 
+        {{-- Articulos --}}
+        <div class="col-5">
+            <div class="row m-3">
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="list-articulos" role="tabpanel" aria-labelledby="capitulo0">
+                        <div class="badge bg-warning text-dark position-relative" >
+                            <h2>Seleccione un capitulo porfavor</h2>
+                        </div>
+                    </div>
+                    @foreach ($capitulos as $cap)
+                    <div class="tab-pane fade" id={{'Articulos'.$cap->idCapitulo}} role="tabpanel" aria-labelledby={{'capitulos'.$cap->idCapitulo}}>         
+                        @foreach ($articulos as $art)                    
+                            @if ($art->idCapitulo === $cap->idCapitulo)
+                                <p>
 
+                                    {{-- PROBANDO EL MODAL --}}
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="{{url('#exampleModal'.$art->idArticulo)}}">
+                                        {{$art->articulo}}
+                                    </button>
+                                      
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="{{'exampleModal'.$art->idArticulo}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">El Titulo</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
 
-      </div>
+                                                <div class="modal-body">
+                                                    {{$art->articulo}}
+                                                </div>
+
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    {{-- AQUI TERMINA EL MODAL --}}
+                                    
+                                </p>
+                            @endif
+                        @endforeach
+                    </div> 
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
     </div>
-
-  </div>
 
   
 
