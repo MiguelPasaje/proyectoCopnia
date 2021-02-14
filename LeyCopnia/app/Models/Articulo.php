40038249213e4e7efaccf3fd9b58ec1c0f121e0d
MiguelPasaje <cartel-dy@hotmail.com>
2021-02-14 11:59:16 -0500

    modelos corregidos

--- LeyCopnia/app/Models/Articulo.php
+++ LeyCopnia/app/Models/Articulo.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Articulo extends Model
 {
     use HasFactory;
+    protected $table = 'Articulos';
     protected $primaryKey = 'idArticulo';
 }
--- LeyCopnia/app/Models/Paragrafo.php
+++ LeyCopnia/app/Models/Paragrafo.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Paragrafo extends Model
 {
     use HasFactory;
+    protected $table = 'Paragrafo';
     protected $primaryKey = 'idParagrafo';
 }
