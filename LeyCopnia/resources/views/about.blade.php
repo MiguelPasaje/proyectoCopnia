427fca4dcf3b83ce2115e69cdd9505004b23d597
MiguelPasaje <cartel-dy@hotmail.com>
2021-02-15 08:58:45 -0500

    img

--- LeyCopnia/resources/views/about.blade.php
+++ LeyCopnia/resources/views/about.blade.php
@@ -92,6 +92,33 @@ p{
 
 </div>
 
+<div class="shadow-lg p-3 mb-5 bg-body rounded " style="margin:5%;">
+
+    <div class="row">
+        <div class="col-sm-4 col-sm-5 col-lg-4 " style="padding: 2%;">
+            <h1 >
+                Juan Felipe Hidalgo
+            </h1>
+    
+            <img src="https://scontent.fclo1-1.fna.fbcdn.net/v/t1.0-9/73372278_3220312498041154_8392663247991341056_o.jpg?_nc_cat=107&ccb=3&_nc_sid=174925&_nc_ohc=fki2BejWvOEAX8IRZos&_nc_ht=scontent.fclo1-1.fna&oh=60e84ec243433235f8172b30b4487f1c&oe=604E913F" alt="">
+        </div>
+        <div class="col-sm-4 col-md-5 offset-md-4 col-lg-4 offset-lg-0" style="padding: 2%;">
+            <h1>
+                Miguel Andres Pasaje
+            </h1>
+    
+            <img src="https://scontent.fclo1-1.fna.fbcdn.net/v/t31.0-8/29060224_1837700169625929_9116675300555721571_o.jpg?_nc_cat=103&ccb=3&_nc_sid=174925&_nc_ohc=GVzgadGZN7AAX_uE8m1&_nc_ht=scontent.fclo1-1.fna&oh=a5c3fc5997fd2e7fa527e56733a5a449&oe=604E2A52" alt="">
+        </div>
+        <div class="col-sm-4 col-md-5 offset-md-2 col-lg-4 offset-lg-0" style="padding: 2%;">
+            <h1>
+                Carlos Andres Gudiño
+            </h1>
+    
+            <img src="https://scontent.fclo1-1.fna.fbcdn.net/v/t1.0-9/117822539_1767195766779646_5060493221132430053_n.jpg?_nc_cat=100&ccb=3&_nc_sid=09cbfe&_nc_ohc=eElHQyxUqY0AX9ORC79&_nc_ht=scontent.fclo1-1.fna&oh=7eda6ba9883d07b57f1534decad41855&oe=60505733" alt="">
+        </div>
+      </div>
+
+</div>
   
 
 @stop
